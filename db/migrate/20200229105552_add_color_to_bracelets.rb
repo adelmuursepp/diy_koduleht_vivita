0f4a7a0f24bba2dde26f3bb75aa8de177a158eb1
Adel Muursepp <adel.muursepp@gmail.com>
2020-02-29 13:27:30 +0200

    Added students model

--- db/migrate/20200229105552_add_color_to_bracelets.rb
+++
@@ -1,5 +0,0 @@
-class AddColorToBracelets < ActiveRecord::Migration[6.0]
-  def change
-    add_column :bracelets, :color, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_110537) do
+ActiveRecord::Schema.define(version: 2020_02_29_112510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2020_02_29_110537) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "color"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
