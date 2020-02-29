dafcd56b2f311242eda3590025d137f0677ebedd
Adel Muursepp <adel.muursepp@gmail.com>
2020-02-29 13:39:42 +0200

    Seeded some

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating students...'
+albert = Student.new(name: "Albert", age: 12)
+albert.save!
+
+aida = Student.new(name: "Aida", age: 9)
+aida.save!
+
+puts 'Students finished!'
+
+
+puts 'Creating bracelets...'
+blue = Bracelet.new(name: "Blue bracele", color: "Blue", price: 5)
+blue.save!
+
+red = Bracelet.new(name: "Red bracelet", color: "Red", price: 4)
+red.save!
+
+puts 'Bracelets finished!'
