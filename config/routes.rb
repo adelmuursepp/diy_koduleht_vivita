2387b17adf09236e05aacc347de3a7fcbcd0a918
Adel Muursepp <adel.muursepp@gmail.com>
2020-02-29 13:44:36 +0200

    Students index

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'bracelets#index'
   get 'bracelets/:id', to: 'bracelets#show'
+  get 'students', to: 'students#index'
 end
